a354fa6bd98e6117980711ca74f028a9c02b03c6
宋峰Feng Song <fengsong@clubfactory.com>
2021-04-02 11:21:13 +0800

    songfeng

--- dataset/preprocessing.py
+++ dataset/preprocessing.py
@@ -12,19 +12,18 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 class CrossData:
     def __init__(self, path_s, path_t, ratio, thre_i, thre_u):
-        self.path_s, self.path_t, self.ratio, self.thre_i, self.thre_u= path_s, path_t, ratio, thre_i, thre_u
+        self.path_s, self.path_t, self.ratio, self.thre_i, self.thre_u = path_s, path_t, ratio, thre_i, thre_u
         self.df_s, self.df_t = self.get_df()
         self.df_s, self.df_t = self.filterout(self.df_s, self.df_t, thre_i, thre_u)
         self.udict, self.idict_s, self.idict_t, self.overlap_user_set = self.convert_idx()
         self.coldstart_user_set, self.common_user_set, self.train_common_s, self.train_common_t, \
-            self.train_s, self.train_t, self.train_cs_s, self.vali, self.test = self.split_train_test()
+        self.train_s, self.train_t, self.train_cs_s, self.vali, self.test = self.split_train_test()
         if args.info: exit()
         self.vocab_dict, self.word_embedding = self.get_w2v()
         self.docu_udict_s, self.docu_idict_s, self.auxiliary_docu_udict_s = \
             self.get_documents(self.train_s, self.coldstart_user_set | self.common_user_set)
         self.docu_udict_t, self.docu_idict_t, self.auxiliary_docu_udict_t = \
             self.get_documents(self.train_t, self.common_user_set)
-
 
     def get_df(self):
         def parse(path):
@@ -60,7 +59,6 @@ class CrossData:
 
         return df_s, df_t
 
-
     def filterout(self, df_s, df_t, thre_i, thre_u):
         index_s = df_s[["overall", "asin"]].groupby('asin').count() >= thre_i
         index_t = df_t[["overall", "asin"]].groupby('asin').count() >= 30
@@ -78,11 +76,10 @@ class CrossData:
 
         return df_s, df_t
 
-
     def convert_idx(self):
         uiterator = count(0)
         udict = defaultdict(lambda: next(uiterator))
-        [udict[user] for user in self.df_s["reviewerID"].tolist()+ self.df_t["reviewerID"].tolist()]
+        [udict[user] for user in self.df_s["reviewerID"].tolist() + self.df_t["reviewerID"].tolist()]
         iiterator_s = count(0)
         idict_s = defaultdict(lambda: next(iiterator_s))
         [idict_s[item] for item in self.df_s["asin"]]
@@ -111,25 +108,26 @@ class CrossData:
         print('Source domain users %d, items %d, ratings %d.' % (self.user_num_s, self.item_num_s, len(self.df_s)))
         print('Target domain users %d, items %d, ratings %d.' % (self.user_num_t, self.item_num_t, len(self.df_t)))
         print('Overlapping users %d (%.3f%%, %.3f%%).' % (
-            self.overlap_num_user, self.overlap_num_user/self.user_num_s*100, self.overlap_num_user/self.user_num_t*100))
+            self.overlap_num_user, self.overlap_num_user / self.user_num_s * 100,
+            self.overlap_num_user / self.user_num_t * 100))
 
         return dict(udict), dict(idict_s), dict(idict_t), overlap_user_set
 
-
     def split_train_test(self):
         random.seed(666)  # 这里的seed保证cold-start users的相同
-        coldstart_user_set = set(random.sample(self.overlap_user_set, int(self.overlap_num_user*0.5)))
-        common_user_all_set = self.overlap_user_set-coldstart_user_set
+        coldstart_user_set = set(random.sample(self.overlap_user_set, int(self.overlap_num_user * 0.5)))
+        common_user_all_set = self.overlap_user_set - coldstart_user_set
         random.seed(2019)
-        common_user_set = set(random.sample(common_user_all_set, int(len(common_user_all_set)*self.ratio)))
+        common_user_set = set(random.sample(common_user_all_set, int(len(common_user_all_set) * self.ratio)))
         train_common_s = self.df_s[self.df_s['uid'].isin(common_user_set)]
         train_common_t = self.df_t[self.df_t['uid'].isin(common_user_set)]
-        train_s = self.df_s[self.df_s['uid'].isin(common_user_all_set-common_user_set).apply(lambda x: not x)]
-        train_t = self.df_t[self.df_t['uid'].isin((common_user_all_set-common_user_set)|coldstart_user_set).apply(lambda x: not x)]
+        train_s = self.df_s[self.df_s['uid'].isin(common_user_all_set - common_user_set).apply(lambda x: not x)]
+        train_t = self.df_t[
+            self.df_t['uid'].isin((common_user_all_set - common_user_set) | coldstart_user_set).apply(lambda x: not x)]
         train_cs_s = self.df_s[self.df_s['uid'].isin(coldstart_user_set)]
 
         random.seed(2019)
-        coldstart_user_vali_set = set(random.sample(coldstart_user_set, int(len(coldstart_user_set)*0.4)))
+        coldstart_user_vali_set = set(random.sample(coldstart_user_set, int(len(coldstart_user_set) * 0.4)))
         coldstart_user_test_set = coldstart_user_set - coldstart_user_vali_set
 
         vali = self.df_t[self.df_t['uid'].isin(coldstart_user_vali_set)]
@@ -140,7 +138,6 @@ class CrossData:
 
         return coldstart_user_set, common_user_set, train_common_s, train_common_t, train_s, train_t, train_cs_s, \
                vali, test
-
 
     def get_w2v(self):
         all_text = self.df_s['reviewText'].tolist()
@@ -158,7 +155,6 @@ class CrossData:
                     word_embedding[idx] = google_model.vectors[google_vocab[word].index]
 
         return vocab_dict, word_embedding
-
 
     def get_documents(self, df, user_set):
         reviews = [list(map(lambda x: self.vocab_dict.get(x, -1), review.split(' '))) for review in df['reviewText']]
@@ -178,16 +174,16 @@ class CrossData:
             row = df_aux[['uid', 'iid', 'overall', 'review_idx']].loc[idx]
             user, item, rating, review = row[0], row[1], row[2], row[3]
 
-            exact_matches = df_aux[(df_aux['iid']==item) & (df_aux['overall']==rating)]
+            exact_matches = df_aux[(df_aux['iid'] == item) & (df_aux['overall'] == rating)]
             if not exact_matches.empty:
                 auxiliary_docu_udict[user].extend(exact_matches.sample(1)['review_idx'].to_list()[0])
                 continue
-            elif not df_aux[(df_aux['iid']==item) & (df_aux['overall']==rating+1)].empty:
-                up_matches = df_aux[(df_aux['iid']==item) & (df_aux['overall']==rating+1)]
+            elif not df_aux[(df_aux['iid'] == item) & (df_aux['overall'] == rating + 1)].empty:
+                up_matches = df_aux[(df_aux['iid'] == item) & (df_aux['overall'] == rating + 1)]
                 auxiliary_docu_udict[user].extend(up_matches.sample(1)['review_idx'].to_list()[0])
                 continue
             else:
-                down_matches = df_aux[(df_aux['iid']==item) & (df_aux['overall']==rating-1)]
+                down_matches = df_aux[(df_aux['iid'] == item) & (df_aux['overall'] == rating - 1)]
                 auxiliary_docu_udict[user].extend(down_matches.sample(1)['review_idx'].to_list()[0])
 
         max_length = 500
@@ -206,10 +202,9 @@ class CrossData:
 
         return cut_docu_udict, cut_docu_idict, cut_auxiliary_docu_udict
 
-
     def dump_pkl(self):
         def extract_ratings(df):
-            ratings = df.apply(lambda x:(x['uid'], x['iid'], x['overall']), axis=1).tolist()
+            ratings = df.apply(lambda x: (x['uid'], x['iid'], x['overall']), axis=1).tolist()
             return ratings
 
         pkl_path = self.path_s.replace(self.path_s.split('/')[-1], 'crossdata_i%d_u%d_%.2f.pkl' %
@@ -226,7 +221,7 @@ class CrossData:
                     ]
             pickle.dump(data, f)
             print('Build data to %s.' % pkl_path)
-            print('='*60)
+            print('=' * 60)
 
 
 if __name__ == '__main__':
